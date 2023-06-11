2488cae3e45394c91e5b9fe1753ff75d93dea07b
Andrés Francisco almarza menjiba <francisco.almarza@gmail.com>
2023-06-11 15:25:33 -0400

    Ajustes para el selector de proyectos

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+console.log('Prueba de que el archivo js está funcionando');
+
+document.getElementById("btn-tarea-1").onclick = function () {
+    location.href = "https://neroangello.github.io/tarea-1-front-end/";
+};
+
+document.getElementById("btn-tarea-2").onclick = function () {
+    console.log('prueba');
+    location.href = "https://neroangello.github.io/tarea-2-front-end/";
+};
+
+document.getElementById("btn-tarea-3").onclick = function () {
+    console.log('prueba');
+    location.href = "https://neroangello.github.io/tarea-3-front-end/";
+};
